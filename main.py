f3abba81246ea04f6208d54d84ebcb9b5bfb77a2
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2021-03-15 17:06:42 +0100

    kocham pyton

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def ascii_to_bin_list(char):
     return [*[0] * (8 - len(bin_list)), *bin_list]
 
 
+def string_to_bin_list(string):
+    bin_list = []
+    for char in string:
+        bin_list += ascii_to_bin_list(char)
+    return bin_list
+
+
 if __name__ == '__main__':
-    print(hMatrix)
+    print(string_to_bin_list('aaa'))
+    print(len(string_to_bin_list('aaa')) / 8)
 
