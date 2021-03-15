e3c9953a7ecc428cab04e8ac1a2641715ff7c789
229871 Dyczka Monika <229871@edu.p.lodz.pl>
2021-03-15 17:42:45 +0100

    nwm jeszcze czy dziala ale cos tam napisalam

--- main.py
+++ main.py
@@ -9,10 +9,55 @@ hMatrix = np.array([[1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
                     [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1]])
 
 
+
+def convert(msg):
+   msgArray = bytearray(msg)
+   msgLength = len(msgArray)
+   encodedMsg = np.zeros((msgLength, 16))
+   for x in range(msgLength):
+       print('jestem glupia')
+
 def ascii_to_bin_list(char):
     bin_list = [int(x) for x in list('{0:0b}'.format(ord(char)))]
     return [*[0] * (8 - len(bin_list)), *bin_list]
 
+def check(vector):
+    if len(vector) != len(hMatrix[0]):
+        print('\nU cannot multiply the matrices')
+        return None
+    row = []
+    checkingArray = [8]
+    for i in range(8):
+        row = hMatrix[i]
+        sum = False
+        for j in range(16):
+            matrixElem = row[j] == 1
+            vectorElem = vector[j] == 1
+            sum ^= matrixElem & vectorElem
+        if sum:
+            checkingArray[i] = 1
+        else:
+            checkingArray[i] = 0
+    return checkingArray
+
+def correct(rMatrix, index, encodedMsg):
+    temp = [8]
+    for i in range(16):
+        j = i + 1
+        for j in range(16):
+            for k in range(8):
+                temp[k] = hMatrix[k][i] ^ hMatrix[k][j]
+            if temp == rMatrix:
+                encodedMsg[index][i] ^= 1
+                encodedMsg[index][j] ^= 1
+                return 2
+    for i in range(16):
+        for j in range(8):
+            temp[j] = hMatrix[j][i]
+        if temp == rMatrix:
+            encodedMsg[index][i] ^= 1
+            return 1
+    return 0
 
 if __name__ == '__main__':
     print(hMatrix)
