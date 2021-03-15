cbe9296d2974a6595c0c6db9c64bb570d7c9ba3a
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2021-03-15 17:01:22 +0100

    zmienione nazwy, zgodnie z PEP

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ hMatrix = np.array([[1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
                     [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1]])
 
 
-def asciitobin(char):
-    binlist = [int(x) for x in list('{0:0b}'.format(ord(char)))]
-    return [*[0] * (8 - len(binlist)), *binlist]
+def ascii_to_bin_list(char):
+    bin_list = [int(x) for x in list('{0:0b}'.format(ord(char)))]
+    return [*[0] * (8 - len(bin_list)), *bin_list]
 
 
 if __name__ == '__main__':
