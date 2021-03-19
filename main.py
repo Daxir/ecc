cc34e67fb32885e58abab8807aee102eb0c01fd5
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2021-03-19 16:33:27 +0100

    Chyba skończone

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import numpy as np
 import os
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
-from PyQt5.QtGui import QPixmap
-from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QGridLayout
+from PyQt5 import QtCore
 from PyQt5.QtGui import QCursor, QIcon
 from bitstring import ConstBitStream
 
@@ -17,44 +16,12 @@ hMatrix = np.array([[1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
                     [1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1]])
 
 
-def convert(msg):
-   msgArray = bytearray(msg)
-   msgLength = len(msgArray)
-   encodedMsg = np.zeros((msgLength, 16))
-   for x in range(msgLength):
-       print('jestem glupia')
-
-
-def flatten(list_of_lists):
-    return [item for sublist in list_of_lists for item in sublist]
-
-
 def ascii_to_bin_list(char):
     bin_list = [int(x) for x in list('{0:0b}'.format(ord(char)))]
     return [*[0] * (8 - len(bin_list)), *bin_list]
 
 
-def check(vector):
-    if len(vector) != len(hMatrix[0]):
-        print('\nU cannot multiply the matrices')
-        return None
-    row = []
-    checkingArray = [8]
-    for i in range(8):
-        row = hMatrix[i]
-        sum = False
-        for j in range(16):
-            matrixElem = row[j] == 1
-            vectorElem = vector[j] == 1
-            sum ^= matrixElem & vectorElem
-        if sum:
-            checkingArray[i] = 1
-        else:
-            checkingArray[i] = 0
-    return checkingArray
-
-
-def checkpython(coded):
+def check(coded):
     checking_arr = []
     for block in coded:
         for vec in hMatrix:
@@ -62,26 +29,26 @@ def checkpython(coded):
     return np.array([checking_arr[i:i + 8] for i in range(0, len(checking_arr), 8)])
 
 
-def correct(rMatrix, index, encodedMsg):
+def correct(r_matrix, index, encoded_msg):
     temp = [None]*8
     for i in range(16):
         for j in range(i+1, 16):
             for k in range(8):
                 temp[k] = hMatrix[k][i] ^ hMatrix[k][j]
-            if temp == rMatrix.tolist():
-                encodedMsg[index][i] ^= 1
-                encodedMsg[index][j] ^= 1
+            if temp == r_matrix.tolist():
+                encoded_msg[index][i] ^= 1
+                encoded_msg[index][j] ^= 1
                 return 2
     for i in range(16):
         for j in range(8):
             temp[j] = hMatrix[j][i]
-        if temp == rMatrix.tolist():
-            encodedMsg[index][i] ^= 1
+        if temp == r_matrix.tolist():
+            encoded_msg[index][i] ^= 1
             return 1
     return 0
 
 
-def string_to_bin_list(string): #to jest w zasadzie prepare tak naprawde to nie
+def string_to_bin_list(string):
     return [char for char in map(ascii_to_bin_list, string)]
 
 
@@ -96,7 +63,6 @@ def encode(string):
 
 
 def decode(bits):
-    print("Bits", bits)
     return ''.join([chr(int(''.join(map(str, sublist[:8])), 2)) for sublist in bits])
 
 
@@ -115,13 +81,30 @@ def write_bits_file(binary_arrays, filename):
         file.write(''.join([str(char) for char in sub]) + " ")
     file.close()
 
-def funfun(encodedMsg):
-    checkarray = checkpython(encodedMsg)
+
+def read_bytes_file(filename):
+    file = open(filename, "r")
+    mess = file.read()
+    file.close()
+    return mess
+
+
+def read_bits_file(filename):
+    file = open(filename, "r")
+    mess = file.read()
+    mess = mess.rstrip()
+    file.close()
+    return list(map(lambda x: [int(c) for c in x], mess.split(sep=' ')))
+
+
+def correct_err(encoded_msg):
+    checkarray = check(encoded_msg)
     errors = 0
-    for i in range(len(encodedMsg)):
+    for i in range(len(encoded_msg)):
         if np.sum(checkarray[i]) > 0:
-            errors += correct(checkarray[i], i, encodedMsg)
-    print(errors)
+            errors += correct(checkarray[i], i, encoded_msg)
+    return errors
+
 
 # Gui zone:
 widgets = {
@@ -130,12 +113,46 @@ widgets = {
     "next": []
 }
 
-names = ["step1.txt", "step2.txt", "step3.txt", "step4.txt"]
+names = ["step1.txt", "step2.txt", "step3.txt", "step4.txt", "step5.txt"]
+texts = ["Krok 1: Otwórz plik i wpisz do niego wiadomość",
+         "Krok 2: W pliku znajdują się zakodowane bajty (rozmiar uległ podwojeniu)",
+         "Krok 3: W pliku znajdują się zakodowane bity, wprowadź błędy",
+         "Krok 4: W pliku znajdują się poprawione odkodowane bity (naprawiono ",
+         "Krok 5: W pliku znajdują się poprawione odkodowane bajty, identyczne jak pierwotna wiadomość"]
 file_openers = [lambda name=name: os.startfile(name) for name in names]
+messages = {
+    "plaintext": "",
+    "encoded": [],
+    "corrected": [],
+    "decoded": ""
+}
 
 
-def stage2():
+def operate(index):
+    if index == 1:
+        messages["plaintext"] = read_bytes_file(names[index - 1])
+        messages["encoded"] = encode(messages["plaintext"])
+        write_bytes_file(messages["encoded"], names[index])
+    elif index == 2:
+        write_bits_file(messages["encoded"], names[index])
+    elif index == 3:
+        messages["encoded"] = read_bits_file(names[index - 1])
+        messages["corrected"] = messages["encoded"]
+        texts[index] += str(correct_err(messages["corrected"])) + ")"
+        write_bits_file(messages["corrected"], names[index])
+    elif index == 4:
+        messages["decoded"] = decode(messages["corrected"])
+        file = open(names[index], "w")
+        file.write(messages["decoded"])
+        file.close()
+    else:
+        return
+
+
+def stage(index):
+    operate(index)
     clear_widgets()
+    frame(texts[index], index)
 
 
 def set_files():
@@ -160,7 +177,6 @@ def clear_widgets():
 def create_button(text, l_margin, r_margin):
     button = QPushButton(text)
     button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-    # button.setFixedWidth(150)
     button.setStyleSheet(
         "*{margin-left: " + str(l_margin) + "px;" +
         "margin-right: " + str(r_margin) + "px;" +
@@ -179,59 +195,29 @@ def create_button(text, l_margin, r_margin):
     return button
 
 
-def frame1(grid):
+def frame(text, index):
     logo = QLabel()
-    logo.setPixmap(QPixmap("logo.png").scaled(357, 85))
-    # logo.setText("Krok 1: Otwórz plik i wpisz do niego wiadomość")
+    logo.setText(text)
     logo.setAlignment(QtCore.Qt.AlignCenter)
+    logo.setWordWrap(True)
     logo.setStyleSheet(
         '''
         font-size: 15px;
-        color: 'white';
+        color: '#f5a7bc';
         margin-bottom: 3px; 
         '''
     )
     widgets["logo"].append(logo)
 
     button = create_button("Otwórz plik", 10, 10)
-    button2 = create_button("Dalej", 10, 10)
+    button.clicked.connect(lambda: file_openers[index]())
+    if index != 4:
+        button2 = create_button("Dalej", 10, 10)
+        button2.clicked.connect(lambda: stage(index + 1))
+    else:
+        button2 = create_button("Zakończ", 10, 10)
+        button2.clicked.connect(lambda: window.close())
 
-    # button = QPushButton("Open file")
-    # button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-    # button.setStyleSheet(
-    #     '''
-    #     *{
-    #         border: 4px solid '#f5a7bc';
-    #         font-size: 15px;
-    #         color: 'white';
-    #         padding: 5px 0;
-    #         margin: 12px 100px;
-    #     }
-    #     *:hover{
-    #         background: '#f5a7bc';
-    #     }
-    #     '''
-    # )
-
-    button.clicked.connect(lambda: file_openers[0]())
-    button2.clicked.connect(stage2)
-    widgets["button"].append(button)
-    widgets["next"].append(button2)
-
-    grid.addWidget(widgets["logo"][-1], 0, 0, 1, 2)
-    grid.addWidget(widgets["button"][-1], 1, 0)
-    grid.addWidget(widgets["next"][-1], 1, 1)
-
-def frame2():
-    logo = QLabel()
-    logo.setPixmap(QPixmap("logo.png").scaled(357, 85))
-    logo.setAlignment(QtCore.Qt.AlignCenter)
-    logo.setStyleSheet("margin-top: 1px;")
-    widgets["logo"].append(logo)
-    button = create_button("Open file", 10, 10)
-    button2 = create_button("Next", 10, 10)
-    button.clicked.connect(lambda: file_openers[1]())
-    button2.clicked.connect(stage2)
     widgets["button"].append(button)
     widgets["next"].append(button2)
 
@@ -255,31 +241,11 @@ class Root(QWidget):
 
 
 if __name__ == '__main__':
-    message = "blackpink"
-    converted = string_to_bin_list(message)
-    encoded = encode(message)
+    app = QApplication(sys.argv)
+    window = Root()
+    grid = QGridLayout()
+    stage(0)
 
-    print(message)
-    print(converted)
-    print(encoded)
-    encoded[0][0] = 1
-    encoded[5][6] = 1
-    encoded[7][8] = 1
-    encoded[7][9] = 0
-    print(encoded)
-    print(funfun(encoded))
-    decoded = decode(encoded)
-    print(decoded)
-    print(encoded)
-
-
-    # os.startfile("gui.txt")
-
-    # app = QApplication(sys.argv)
-    # window = Root()
-    # grid = QGridLayout()
-    # frame1(grid)
-    #
-    # window.setLayout(grid)
-    # window.show()
-    # sys.exit(app.exec())
+    window.setLayout(grid)
+    window.show()
+    sys.exit(app.exec())
